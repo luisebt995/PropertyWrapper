975d81261c3de62f7cc508327980c5e6c8d9cffd
Luis Bustamante <luisebt995@gmail.com>
2024-03-27 15:36:39 +0100

    Initial Commit

---
+++ PropertyWrapper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PropertyWrapper
+//
+//  Created by Luis on 27/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PropertyWrapper/PropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrapperApp.swift
+//  PropertyWrapper
+//
+//  Created by Luis on 27/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
